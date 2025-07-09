76d118eb828ef838ac0646a604df2717eaf137d9
kerry <Careyk896>
2025-07-09 17:09:35 +0100

    Working with props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div>
-    <UserProfile name= "Alice" age= "25" bio= "Loves Hiking and Photography"/>
+    <UserProfile name= "Alice" age= "25" bio= "Loves hiking and photography" />
     </div>
   );
 }
